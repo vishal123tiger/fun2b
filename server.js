e4b0705828a965ded64a332aa836464b4a8a6414
vikrant9031 <vikratsingh769@gmail.com>
2018-12-29 18:59:14 +0530

    s

--- server.js
+++ server.js
@@ -44,15 +44,43 @@ if(isValid){
 })
 })
 //..........................get Register
-app.get('/res',(req,res)=>{
-	postgres.select('*').from('register').then(data=>{
 
 
 
-			res.json(data);
+
+
+
+//check...................................................
+app.post('/check',(req,res)=>{
+
+	const{rid} = req.body;
+	postgres.select('*').from('requests').where({rid:rid,status:0}).then(data=>{
+data.map((user,i)=>{
+	postgres.select('*').from('register').where({id:user.sid}).then(data=>{
+		res.json(data);
+	})
+})
+
+
+
+	})
+})
+//request............................................................................................................
+app.post('/requests',(req,res)=>{
+	const{sid,rid} = req.body;
+
+postgres.insert({sid:sid,rid:rid,status:0}).into('requests').returning('*').then(data=>{
+	res.json(data);
+})
 
 
 })
+//............................................................
+app.post('/getr',(req,res)=>{
+const {rid} = req.body;
+	postgres.select('*').from('register').where({id:9}).then(data=>{
+		res.json(data);
+	})
 })
 //register................................................................................................................................................
 app.post('/register',(req,res)=>{
@@ -81,10 +109,15 @@ app.post('/post',(req,res,next)=>{
 const {filename,uid} = req.body;
 console.log(parseInt(req.body.uid));
 const time = new Date();
-const h = time.getHours();
-const m = time.getMinutes();
-const s = time.getSeconds();
-const change = h+":"+m+":"+s;
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+var h = time.getHours();
+var m = time.getMinutes();
+var s = time.getSeconds();
+var change = h+":"+m+":"+s;
+var y = time.getFullYear();
+var d = time.getDate();
+var mi = months[time.getMonth()];
+
 let imageFile = req.files.file;
 name = imageFile.name;
 
@@ -93,7 +126,7 @@ imageFile.mv(`${__dirname}/public/${name}`, function(err) {
 		return res.status(500).send(err);
 		`public/${data[0].img}`
 	}else{
-	postgres.insert({likes:0,user_id:uid,des:filename,time:change,img:`public/${name}`})
+	postgres.insert({times:time,year:y,month:mi,day:d,comments:0,likes:0,user_id:uid,des:filename,time:change,img:`public/${name}`})
 	.into('feed')
 	.returning('*').then(data=>{
 		res.json({file:data[0].img,value:data[0].des});
@@ -129,12 +162,20 @@ console.log(name);
 
 //feed getting...........................................................................................................................................
 app.get('/get',(req,res)=>{
-postgres.select('*').from('feed').then(data=>{
+
+postgres.select('*').from('feed').orderBy('times','desc').then(data=>{
+data.map((user,i)=>{
+
+	postgres.select('name').from('register').where({id:user.user_id}).then(values=>{
+		array.push(values);
+	})
+
+
+})
 
 
 
-		res.json(data);
-
+res.json({file:data,files:array});
 
 
 
@@ -163,16 +204,70 @@ app.get('/likes',(req,res)=>{
 			})
 		})
 	})
+})
 
-//postgres('feed').where({cid:1}).update({likes:2}).returning('*').then(data=>res.json(data));
+//////////////////////
+app.post('/res1',(req,res)=>{
+
+const {rid} = req.body;
+
+	postgres.select('*').from('requests').where({sid:rid,status:1}).orWhere({rid:rid,status:1}).then(data=>{
+data.map((user,i)=>{
+	if(user.sid == rid){
+	postgres.select('*').from('register').where({id:user.rid}).then(data=>{
+		res.json(data);
+	})
+	}else{
+		postgres.select('*').from('register').where({id:user.sid}).then(data=>{
+			res.json(data);
+		})
+	}
+})
+})
+})
+	//....................
+	app.post('/request1',(req,res)=>{
+		const {rid,sid} = req.body;
+console.log(sid);
+
+
+postgres('requests').where({sid:sid,rid:rid,status:0}).update({status:1}).returning('*').then(data=>{
+res.json(data);
+});
+
+
+
+	})
+//........................
+app.get('/comm',(req,res)=>{
+	postgres.select('*').from('feed').then(data=>{
+		data.map((users,i)=>{
+			postgres.select('commentbyid','user_id').from('comments').where({user_id:users.cid}).then(values=>{
+				if(values.length>0){
+			 var count = values.length;
+				i = values[0].user_id;
+			 postgres('feed').where({cid:i}).update({comments:count}).returning('*').then(console.log(data));
+		 }else{
+			 console.log("empty");
+		 }
+
+
+
+
+
+			})
+		})
 
 })
+})
+//postgres('feed').where({cid:1}).update({likes:2}).returning('*').then(data=>res.json(data));
+
 //like.................................................................................................................................................
 app.post('/like',(req,res)=>{
 const {conid,likebyid} = req.body;
 postgres.select('*').from('likepost').where({cont_id:conid,user_id:likebyid}).then(data=>{
 	if(data.length>0){
-		console.log("error");
+	res.json("error");
 	}else{
 		postgres.insert({user_id:likebyid,cont_id:conid}).into("likepost").returning('*').then(data=>{
 			res.json(data);
@@ -185,7 +280,13 @@ postgres.select('*').from('likepost').where({cont_id:conid,user_id:likebyid}).th
 
 
 })
+//..........................................................comment................................
+app.post('/comment',(req,res)=>{
+	const{conid,commentbyid,comment} = req.body;
+	postgres.insert({commentbyid:commentbyid,user_id:conid,comments:comment})
+	.into("comments").returning('*').then(data=>res.json(data));
 
+})
 //host.......................................................................................................................................................
 app.listen(3001,()=>{
   console.log('running 3001');
